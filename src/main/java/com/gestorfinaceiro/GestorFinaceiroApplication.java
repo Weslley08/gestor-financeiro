db128650debbd3f22408a3a037228170f220a3b2
Weslley08 <83292843+Weslley08@users.noreply.github.com>
2021-09-14 10:23:06 -0300

    first commit

---
+++ src/main/java/com/gestorfinaceiro/GestorFinaceiroApplication.java
@@ -0,0 +1,13 @@
+package com.gestorfinaceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorFinaceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorFinaceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestorfinaceiro/GestorFinaceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestorfinaceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorFinaceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
