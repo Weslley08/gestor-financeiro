6a1c5c71acb9a3b8af828cc0ef23de9a17667bba
Weslley08 <83292843+Weslley08@users.noreply.github.com>
2021-09-15 13:23:35 -0300

    Removendo unused imports

--- src/main/java/com/gestorfinaceiro/models/Clientes.java
+++ src/main/java/com/gestorfinaceiro/models/Clientes.java
@@ -1,12 +1,10 @@
 package com.gestorfinaceiro.models;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
